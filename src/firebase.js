37e8e8f4fafdd6843e998e8f44dbde1e5a953253
shin <shin5513976@naver.com>
2021-04-12 17:35:30 +0900

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/auth"
+import "firebase/database"
+import "firebase/storage"
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBqST3Y4hYN-ltsF_jShdyhsyZjM-Xy_Uk",
+    authDomain: "shoping-89ae6.firebaseapp.com",
+    projectId: "shoping-89ae6",
+    storageBucket: "shoping-89ae6.appspot.com",
+    messagingSenderId: "210017108925",
+    appId: "1:210017108925:web:40554d86b35e105b9e48d4",
+    measurementId: "G-9WC1PCZJGZ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  //firebase.analytics();
+</script>
