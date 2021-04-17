8e92d558a2dd1280d350be37407b10dcece8c15c
shin <shin5513976@naver.com>
2021-04-17 19:00:29 +0900

    all

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -121,6 +121,40 @@ const [products] = useState([
     }
 ])
 
+const [products1] = useState([
+    {
+        name : 'Intel CPU i7',
+        cost : '400,000',
+        core : '8코어',
+        thread : '8스레드',
+        g : '9세대',
+        image : process.env.PUBLIC_URL + '/cpu_test_image.png'
+    },
+    {
+        name : 'Intel CPU i9',
+        cost : '400,000',
+        core : '8코어',
+        thread : '8스레드',
+        g : '9세대',
+        image : process.env.PUBLIC_URL + '/cpu_test_image.png'
+    },
+    {
+        name : 'Intel CPU i5',
+        cost : '400,000',
+        core : '8코어',
+        thread : '8스레드',
+        g : '9세대',
+        image : process.env.PUBLIC_URL + '/cpu_test_image.png'
+    },
+    {
+        name : 'Intel CPU Pantium Gold',
+        cost : '400,000',
+        core : '8코어',
+        thread : '8스레드',
+        g : '9세대',
+        image : process.env.PUBLIC_URL + '/cpu_test_image.png'
+}
+])
 
 const [products2] = useState([
     {
@@ -263,8 +297,8 @@ const navigateTo = (nextPage)=>{
             {/* 그리고 버튼도 만들어 놨는데 저거를 이제 구매,장바구니랑 연결을 해야되는데 DB영역이니 버튼만 만들어 놓음. */}
             {/* 그리고 장바구니 추가하면 혹시 가능하면 NavBar에 있는 장바구니에 알림 표시기능 있으면 좋을듯 */}
 
-            <Tabs defaultActiveKey="cpu" id="uncontrolled-tab-example">
-                <Tab eventKey="cpu" title="CPU">
+            <Tabs defaultActiveKey="all" id="uncontrolled-tab-example">
+                <Tab eventKey="all" title="all">
                     {/* 여기는 CPU Tab 부분 */}
                     <ListGroup variant="flush">
                         <ListGroup.Item id="cpu1">
@@ -430,7 +464,195 @@ const navigateTo = (nextPage)=>{
                         </ListGroup.Item>
                     </ListGroup>
                 </Tab>
+                
+                <Tab eventKey="cpu" title="CPU">
+                    {/* 여기는 GPU Tab 부분 */}
 
+                    <ListGroup variant="flush">
+                        <ListGroup.Item id="cpu1">
+                            <table class="table table-hover">
+                                <tr>
+                                <div>
+                                {products1.map((product1, idx)=>(
+                                    <div className="product2" key={idx}>
+                                        <img src={product1.image} alt={product1.name}/>
+                                        <h3>{product1.name}</h3>
+                                        <h4>{product1.cost}</h4>
+                                        <h4>{product1.core}</h4>
+                                        <h4>{product1.thread}</h4>
+                                        <h4>{product1.g}</h4>
+                                        <button onClick={()=>addToCart(product1)}>장바구니 추가({cart.length})</button>
+
+                                </div>
+                                ))}
+                                </div>
+                                </tr>
+                            </table>
+
+                            {/* 위의 이미지는 public폴더에 있습니다. public에서 이미지를 가져오려면 process.env.PUBLIC_URLL을 사용해야됩니다. */}
+                            {/* 그리고 주석 단축키는 주석처리할 문장 앞 커서에서 ctrl + / 입니다. 이거 편함 by 차재현 21-04-08*/}
+                            {/* 우선 각 상품 리스트출력 구조는 얼추 된듯. 각 조건에 맞는 상품들을 어떻게 불러올지가 문제..*/}
+                            {/* 그리고 버튼도 만들어 놨는데 저거를 이제 구매,장바구니랑 연결을 해야되는데 DB영역이니 버튼만 만들어 놓음. */}
+                            {/* 그리고 장바구니 추가하면 혹시 가능하면 NavBar에 있는 장바구니에 알림 표시기능 있으면 좋을듯 */}
+
+                        </ListGroup.Item>
+                        <ListGroup.Item id="gpu2">
+                            <table class="table table-hover">
+                                <tr>
+                                    <td class="pricewidth" rowspan="4">
+                                        <img
+                                            src={process.env.PUBLIC_URL + '/gpu_test_image.png'}
+                                            width='200'
+                                            height='200'
+                                            alt="test_image"/>
+
+                                    </td>
+                                    <td colspan="3">
+                                        <h4>제품명 : NVidia GeForce RTX 2080
+                                        </h4>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="3">
+                                        <span>상세 사양</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>코어 : 8코어</td>
+                                    <td>스레드 : 8스레드</td>
+                                    <td>세대 : 9세대</td>
+                                </tr>
+                                <tr>
+                                    <td colspan="3">
+                                        <div class="container">
+                                            <div class="row">
+                                                <div class="col-7">
+                                                    <h2>가격 : 400,000</h2>
+                                                </div>
+                                                <div class="col-2">
+                                                    <React.Fragment>
+                                                        <button type="button" class="btn btn-outline-primary" onClick={openModal}>구매하기</button>
+                                                        <Modal open={modalOpen} close={closeModal} header="구매 페이지"></Modal>
+                                                        {/* header 부분에 텍스트를 입력한다. */}
+
+                                                        {/* Modal.js <main> { props.children } </main>에 내용이 입력된다.  */}
+                                                    </React.Fragment>
+                                                </div>
+                                                <div class="col-3">
+                                                    <button type="button" class="btn btn-outline-warning">장바구니 추가</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+
+                                </tr>
+                            </table>
+                        </ListGroup.Item>
+                        <ListGroup.Item id="gpu3">
+                            <table class="table table-hover">
+                                <tr>
+                                    <td class="pricewidth" rowspan="4">
+                                        <img
+                                            src={process.env.PUBLIC_URL + '/gpu_test_image.png'}
+                                            width='200'
+                                            height='200'
+                                            alt="test_image"/>
+
+                                    </td>
+                                    <td colspan="3">
+                                        <h4>제품명 : NVidia GeForce GTX 1660 super</h4>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="3">
+                                        <span>상세 사양</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>코어 : 8코어</td>
+                                    <td>스레드 : 8스레드</td>
+                                    <td>세대 : 9세대</td>
+                                </tr>
+                                <tr>
+                                    <td colspan="3">
+                                        <div class="container">
+                                            <div class="row">
+                                                <div class="col-7">
+                                                    <h2>가격 : 400,000</h2>
+                                                </div>
+                                                <div class="col-2">
+                                                    <React.Fragment>
+                                                        <button type="button" class="btn btn-outline-primary" onClick={openModal}>구매하기</button>
+                                                        <Modal open={modalOpen} close={closeModal} header="구매 페이지"></Modal>
+                                                        {/* header 부분에 텍스트를 입력한다. */}
+
+                                                        {/* Modal.js <main> { props.children } </main>에 내용이 입력된다.  */}
+                                                    </React.Fragment>
+                                                </div>
+                                                <div class="col-3">
+                                                    <button type="button" class="btn btn-outline-warning">장바구니 추가</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+
+                                </tr>
+                            </table>
+
+                        </ListGroup.Item>
+                        <ListGroup.Item id="gpu4">
+                            <table class="table table-hover">
+                                <tr>
+                                    <td class="pricewidth" rowspan="4">
+                                        <img
+                                            src={process.env.PUBLIC_URL + '/gpu_test_image.png'}
+                                            width='200'
+                                            height='200'
+                                            alt="test_image"/>
+
+                                    </td>
+                                    <td colspan="3">
+                                        <h4>제품명 : AMD Radeon pro</h4>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="3">
+                                        <span>상세 사양</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>코어 : 8코어</td>
+                                    <td>스레드 : 8스레드</td>
+                                    <td>세대 : 9세대</td>
+                                </tr>
+                                <tr>
+                                    <td colspan="3">
+                                        <div class="container">
+                                            <div class="row">
+                                                <div class="col-7">
+                                                    <h2>가격 : 400,000</h2>
+                                                </div>
+                                                <div class="col-2">
+                                                    <React.Fragment>
+                                                        <button type="button" class="btn btn-outline-primary" onClick={openModal}>구매하기</button>
+                                                        <Modal open={modalOpen} close={closeModal} header="구매 페이지"></Modal>
+                                                        {/* header 부분에 텍스트를 입력한다. */}
+
+                                                        {/* Modal.js <main> { props.children } </main>에 내용이 입력된다.  */}
+                                                    </React.Fragment>
+                                                </div>
+                                                <div class="col-3">
+                                                    <button type="button" class="btn btn-outline-warning">장바구니 추가</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+
+                                </tr>
+                            </table>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Tab>
                 <Tab eventKey="gpu" title="GPU">
                     {/* 여기는 GPU Tab 부분 */}
 
